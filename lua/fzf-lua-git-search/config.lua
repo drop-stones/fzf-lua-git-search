ea4169ed1413d92bf195bd32ddc707c1105236a6
drop-stones <55022619+drop-stones@users.noreply.github.com>
2025-04-10 23:27:00 +0900

    feat(grep): Add windows support to fn_transform_cmd shell escaping (#1)

--- lua/fzf-lua-git-search/config.lua
+++ lua/fzf-lua-git-search/config.lua
@@ -4,6 +4,17 @@ M.git_grep = {
   cmd = "git grep --ignore-case --extended-regexp --line-number --column --color=always --untracked",
   winopts = { title = " Git Grep " },
   fn_transform_cmd = function(query, cmd, _)
+    ---Escape a shell argument, with Windows quoting support
+    ---@param shell_arg string
+    ---@return string
+    local function shellescape(shell_arg)
+      if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+        return string.format('^"%s^"', shell_arg)
+      else
+        return string.format("'%s'", shell_arg)
+      end
+    end
+
     -- Extract search query and glob string separated by '--'
     local search_query, glob_str = query:match("(.-)%s-%-%-(.*)")
 
@@ -16,15 +27,15 @@ M.git_grep = {
     for pathspec in glob_str:gmatch("%S+") do
       if pathspec:sub(1, 1) == "!" then
         -- Convert '!pattern' to git's exclude pathspec
-        table.insert(pathspecs, string.format("':(exclude)%s'", pathspec:sub(2)))
+        table.insert(pathspecs, shellescape(":(exclude)" .. pathspec:sub(2)))
       else
         -- Wrap regular pathspec in single quotes
-        table.insert(pathspecs, string.format("'%s'", pathspec))
+        table.insert(pathspecs, shellescape(pathspec))
       end
     end
 
     -- Assemble the final git grep command
-    local new_cmd = string.format("%s %s %s", cmd, vim.fn.shellescape(search_query), table.concat(pathspecs, " "))
+    local new_cmd = string.format("%s %s -- %s", cmd, shellescape(search_query), table.concat(pathspecs, " "))
     return new_cmd, search_query
   end,
 }
