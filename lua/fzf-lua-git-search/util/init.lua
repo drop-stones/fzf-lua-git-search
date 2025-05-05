49d7829b73d00e52811e02f5f8e30030f700e603
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-05-05 19:40:02 +0900

    feat(util): Add utility function to escape args for shell

--- lua/fzf-lua-git-search/util/init.lua
+++ lua/fzf-lua-git-search/util/init.lua
@@ -1,4 +1,5 @@
 return {
   resolve_cwd = require("fzf-lua-git-search.util.resolve_cwd"),
   get_plugin_root = require("fzf-lua-git-search.util.get_plugin_root"),
+  shellescape = require("fzf-lua-git-search.util.shellescape"),
 }
